ef785341b9a9deadf6c7d401dcf64134dbb02a22
danya <reactive722@gmail.com>
2025-11-16 13:48:19 +0300

    my_lcg

--- src/generators.cpp
+++ src/generators.cpp
@@ -3,7 +3,32 @@
 #include <random>
 
 #include "solution.hpp"
-#include "own_gen.cpp"
+
+class my_lcg {
+public:
+    using result_type = uint32_t;
+
+private:
+    result_type state;
+
+public:
+    static constexpr result_type multiplier = 16807;
+    static constexpr result_type modulus    = 2147483647; // 2^31 - 1
+
+    // Требование: должен быть конструктор от seed
+    explicit my_lcg(result_type seed = 1) : state(seed) {}
+
+    // Требование: min() и max()
+    static constexpr result_type min() { return 1; }
+    static constexpr result_type max() { return modulus - 1; }
+
+    // Требование: operator()
+    result_type operator()() {
+        state = (uint64_t(state) * multiplier) % modulus;
+        return state;
+    }
+};
+
 
 Status generate_bits(size_t n, uint32_t seed, uint32_t* result) {
     my_lcg gen(seed);
