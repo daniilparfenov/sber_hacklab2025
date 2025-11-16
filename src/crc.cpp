fbcd4195c643fffb80c235dc92f9336bade8a282
Shon Qwezon <shonzbso@gmail.com>
2025-11-16 12:39:37 +0300

    update

--- src/crc.cpp
+++ src/crc.cpp
@@ -1,19 +1,15 @@
 #include <cstddef> 
 #include <cstdint>
 #include <cmath>
+#include <nmmintrin.h>
 
 #include "solution.hpp"
 
 Status crc32(const uint8_t* data, size_t data_len, uint32_t* result) {
     uint32_t crc = 0xFFFFFFFF;
-    
-    for (size_t i = 0; i < data_len; i++) {
-        crc ^= data[i];
-        for (int j = 0; j < 8; j++)
-            crc = (crc >> 1) ^ (0xEDB88320 & -(crc & 1));
+    for (size_t i = 0; i < data_len; ++i) {
+        crc = _mm_crc32_u8(crc, data[i]);
     }
-
     *result = ~crc;
-    
     return STATUS_OK;
 }
